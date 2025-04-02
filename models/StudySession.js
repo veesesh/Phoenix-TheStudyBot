cec15bc61e2f8bf93418421898e802fde6dbbb21
Vee <vee1912566@gmail.com>
2025-04-02 20:57:23 +0530

    changed schema

--- models/StudySession.js
+++ models/StudySession.js
@@ -6,9 +6,12 @@ mongoose.connect(MONGO_URI);
 const studySessionSchema = new mongoose.Schema({
   userId: String, // Discord User ID
   startTime: Date, // When the session started
+  originalStartTime: Date,
   endTime: Date, // When the session ended (null if ongoing)
-  duration: Number, // Total time in minutes (calculated when session ends)
+  totalDuration: Number, // Total time in minutes (calculated when session ends)
   status: String, // "ongoing", "paused", or "completed"
+  pausedDuration: Number,
+  duration: Number,
 });
 
 const StudySession = mongoose.model("StudySession", studySessionSchema);
