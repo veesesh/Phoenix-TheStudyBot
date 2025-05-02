9af83b3f6c5c56579c7b0741d927c650397eef6a
Vee <vee1912566@gmail.com>
2025-05-03 00:21:12 +0530

    Command: Add /time to show session duration

--- commands/utility/time.js
+++ commands/utility/time.js
@@ -15,13 +15,30 @@ module.exports = {
       return interaction.reply("❌ No active session found.");
     }
 
-    const now = new Date();
-    const totalTime = (now - session.startTime) / 1000;
-    session.totalDuration = +totalTime;
+    let totalseconds;
+    if (session.status === "paused") {
+      totalseconds = session.totalDuration || 0;
+      // no change in total duration
+    } else if (session.status === "ongoing") {
+      const now = new Date();
 
-    const h = Math.floor(session.totalDuration / 3600);
-    const m = Math.floor((session.totalDuration % 3600) / 60);
-    const s = Math.floor(session.totalDuration % 60);
-    await interaction.reply(`Time studied so far: **${h}h ${m}m ${s}s*`);
+      console.log({
+        z: session.startTime,
+        x: (now - session.startTime) / 1000,
+        y: session.totalDuration,
+      });
+      const totalTime = (now - session.startTime) / 1000;
+      totalseconds = (session.totalDuration || 0) + totalTime;
+      console.log({
+        z: session.startTime,
+        x: totalTime,
+        y: session.totalDuration,
+      });
+    }
+
+    const h = Math.floor(totalseconds / 3600);
+    const m = Math.floor((totalseconds % 3600) / 60);
+    const s = Math.floor(totalseconds % 60);
+    await interaction.reply(`Time studied so far: **${h}h ${m}m ${s}s**`);
   },
 };
