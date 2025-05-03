f116ed779e8bc0bdaf539e91c275e9139890fe4c
Vee <vee1912566@gmail.com>
2025-05-03 13:15:58 +0530

    Command: Add /cancel to terminate active study session

--- commands/utility/cancel.js
+++ commands/utility/cancel.js
@@ -1,10 +1,29 @@
 const { SlashCommandBuilder } = require("discord.js");
+const StudySession = require("../../models/StudySession.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("cancel")
     .setDescription("Cancels the session."),
   async execute(interaction) {
-    await interaction.reply("cancelled!");
+    const userId = interaction.user.id;
+
+    const session = await StudySession.findOne({
+      userId,
+      status: { $in: ["ongoing", "paused"] },
+    });
+    if (!session) {
+      return interaction.reply("❌ No active session found.");
+    }
+
+    session.status = "cancelled";
+    session.startTime = null;
+    session.totalDuration = 0;
+
+    await session.save();
+
+    await interaction.reply(
+      "❌ Your study session has been cancelled and discarded."
+    );
   },
 };
