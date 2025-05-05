09729c719ecd43afd9458a0e19af3a3751d0f205
Vee <vee1912566@gmail.com>
2025-05-06 00:45:57 +0530

    feat: implement /clear command to reset user session to default

---
+++ commands/utility/clear.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("discord.js");
+const StudySession = require("../../models/StudySession.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Clears your stats"),
+  async execute(interaction) {
+    const userId = interaction.user.id;
+
+    const session = await StudySession.findOne({
+      userId,
+      status: { $in: ["ongoing", "paused", "ended"] },
+    });
+
+    if (!session) {
+      return interaction.reply("There is no session(s) to clear");
+    }
+
+    session.XP = 0;
+    session.Level = 0;
+    session.log = {};
+    session.status = "ended";
+    session.totalDuration = 0;
+    session.pausedDuration = 0;
+    session.duration = 0;
+    session.startTime = null;
+    session.endTime = null;
+    session.originalStartTime = null;
+    session.record_time = null;
+
+    await session.save();
+    await interaction.reply(`cleared`);
+  },
+};
