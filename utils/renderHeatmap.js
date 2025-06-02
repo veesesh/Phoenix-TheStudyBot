2e18720a405b0e09e856415dad84f7b88e24069b
Vee <vee1912566@gmail.com>
2025-06-02 10:50:09 +0530

    fix: beautify the renderheatmap canva

--- utils/renderHeatmap.js
+++ utils/renderHeatmap.js
@@ -4,32 +4,34 @@ const dayjs = require("dayjs");
 function getColor(seconds) {
   const hours = seconds / 3600;
 
-  if (hours === 0) return "#ebedf0";
-  if (hours < 1) return "#c6e48b";
-  if (hours < 2) return "#7bc96f";
-  if (hours < 4) return "#239a3b";
-  return "#196127";
+  if (hours === 0) return "#161b22";
+  if (hours < 1) return "#0e4429";
+  if (hours < 2) return "#006d32";
+  if (hours < 4) return "#26a641";
+  return "#39d353";
 }
 
 module.exports = function renderHeatmap(log) {
-  const width = 400;
-  const height = 150;
+  const width = 288;
+  const height = 157;
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  const start = dayjs().subtract(29, "day");
+  const start = dayjs().subtract(119, "day");
   const daySize = 15;
   const padding = 4;
+  const leftpadding = 4;
+  const toppadding = 4;
 
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, width, height);
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 120; i++) {
     const date = start.add(i, "day").format("YYYY-MM-DD");
     const seconds = log.get(date) || 0;
 
-    const x = (i % 15) * (daySize + padding);
-    const y = Math.floor(i / 15) * (daySize + padding);
+    const x = leftpadding + (i % 15) * (daySize + padding);
+    const y = toppadding + Math.floor(i / 15) * (daySize + padding);
 
     ctx.fillStyle = getColor(seconds);
     ctx.fillRect(x, y, daySize, daySize);
